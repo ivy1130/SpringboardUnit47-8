371d9a457ff1b803756ba171625f6e99a1003440
Ivy <ivy@onprem.com>
2022-06-30 15:45:49 +0200

    Completed assignment.

--- tree.js
+++ tree.js
@@ -15,20 +15,69 @@ class Tree {
   /** sumValues(): add up all of the values in the tree. */
 
   sumValues() {
-    
+    let toVisitStack = [this.root];
+    let sum = 0
+
+    if (this.root === null) {
+      return 0
+    }
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      sum += current.val
+      if (current.children) {
+        for (let child of current.children) 
+        toVisitStack.push(child)
+      }
+    }
+    return sum
   }
 
   /** countEvens(): count all of the nodes in the tree with even values. */
 
   countEvens() {
+    let toVisitStack = [this.root];
+    let evenCount = 0
 
+    if (this.root === null) {
+      return 0
+    }
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      if (current.val % 2 === 0) {
+        evenCount++
+      }
+      if (current.children) {
+        for (let child of current.children) 
+        toVisitStack.push(child)
+      }
+    }
+    return evenCount
   }
 
   /** numGreater(lowerBound): return a count of the number of nodes
    * whose value is greater than lowerBound. */
 
   numGreater(lowerBound) {
+    let toVisitStack = [this.root];
+    let count = 0
 
+    if (this.root === null) {
+      return 0
+    }
+
+    while (toVisitStack.length) {
+      let current = toVisitStack.pop();
+      if (current.val > lowerBound) {
+        count++
+      }
+      if (current.children) {
+        for (let child of current.children) 
+        toVisitStack.push(child)
+      }
+    }
+    return count
   }
 }
 
